b9f6610723ab525e40c69e8d6f5ab65a3defe1f3
Dmitry Minaev <minaevd@gmail.com>
2015-01-22 21:12:19 +0000

    fixed

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var redis = require('redis');
 var url = require('url');
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
 var credis = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-client.auth(redisURL.auth.split(":")[1]);
+credis.auth(redisURL.auth.split(":")[1]);
 var compress = require('compression')();
 
 io.set('origins', '*:*');
